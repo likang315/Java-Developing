31969ab90885c6ec07a8b2bd8c56522e7ab5dba1
likang315 <13571887352@163.com>
2022-03-16 10:55:54 +0800

    feature: springBoot package jar and war

--- atlantis/atlantis-zeus/src/test/java/com/atlantis/atlantiszeus/AtlantisZeusApplicationTests.java
+++ atlantis/atlantis-zeus/src/test/java/com/atlantis/atlantiszeus/AtlantisZeusApplicationTests.java
@@ -1,13 +1,29 @@
 package com.atlantis.atlantiszeus;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+/**
+ * 单元测试
+ *
+ * @author likang02@corp.netease.com
+ * @date 2021-08-22 16:11
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class AtlantisZeusApplicationTests {
 
+	/**
+	 * 单元测试
+	 */
 	@Test
 	void contextLoads() {
+		log.warn("AtlantisZeusApplicationTests_contextLoads: test !!!");
 	}
 
 }
